f8a13957580e53833297a959ef36158ebe9c004b
Meonu <yeonugim42@gmail.com>
2023-11-10 16:41:26 +0900

    modify admin system

--- src/mall/Admin.java
+++ src/mall/Admin.java
@@ -4,4 +4,12 @@ import mgr.Manager;
 
 public class Admin {
     static Manager userMgr = new Manager();
+    public void adminPage(){
+        if(!Login.currentUser.id.equals("admin")){
+            System.out.println("Permission denied");
+            return;
+        }
+        //TODO : make manage page for admin
+        System.out.println("Hello admin");
+    }
 }
