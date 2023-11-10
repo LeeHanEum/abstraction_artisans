c59cd6fe2f732101dfabf07c28a3c9074b87b285
익호 <dldlrgh28@gmail.com>
2023-11-10 22:57:54 +0900

    feat:user

--- src/mall/User.java
+++ src/mall/User.java
@@ -1,4 +1,35 @@
 package mall;
 
-public class User {
+import mgr.Manageable;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User implements Manageable{
+    private String id;
+    private String pwd;
+    private String name;
+    private String tel;
+    private String address;
+    static ArrayList<Manageable> userList = new ArrayList<>();
+    @Override
+    public void read(Scanner scan) {
+        id = scan.next();
+        pwd = scan.next();
+        name = scan.next();
+        tel = scan.next();
+        address = scan.next();
+    }
+    @Override
+    public boolean matches(String kwd) {
+        if (id.equals(kwd))
+            return true;
+        return false;
+    }
+    @Override
+    public void print() {
+        System.out.format("[%s] (%s) %s %s (%s)\n", id, pwd, name, tel, address);
+    }
+
+
 }
