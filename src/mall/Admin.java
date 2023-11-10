0026def8a41be21f293b722c4a6d36477b0f05f9
Meonu <yeonugim42@gmail.com>
2023-11-10 16:43:11 +0900

    add commnet in admin cals

--- src/mall/Admin.java
+++ src/mall/Admin.java
@@ -4,6 +4,7 @@ import mgr.Manager;
 
 public class Admin {
     static Manager userMgr = new Manager();
+    //admin의 user 관리 페이지
     public void adminPage(){
         if(!Login.currentUser.id.equals("admin")){
             System.out.println("Permission denied");
