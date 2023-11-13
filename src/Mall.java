6e756f34e7d08105ce82cd4d647219739b15ee87
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-13 16:36:01 +0900

    feat : Mall menu 구현

--- src/Mall.java
+++ src/Mall.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Mall {
-    public static void main(String[] args) {
-        System.out.println("Hello, Mall!");
+
+    private static Mall instance;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    // 싱글톤 구현
+    private static Mall getInstance(){
+        if(instance == null)
+            instance = new Mall();
+        return instance;
     }
+
+    // menu enum 필요
+    private static int menuSelect(){
+        System.out.println("### 메뉴 ###");
+        System.out.println("1. 로그인");
+        System.out.println("2. 종료");
+        return scanner.nextInt();
+    }
+
+    private void run(int menuSelect){
+
+    }
+
+    public static void main(String[] args) {
+        Mall.getInstance().run(menuSelect());
+    }
+
 }
