5fa09e2b618cca2dc2a66f7ee236b87b4bdda706
Meonu <109051879+Meonu@users.noreply.github.com>
2023-11-17 09:01:02 +0900

    make encrypt logic

--- src/Main/mall/Password.java
+++ src/Main/mall/Password.java
@@ -6,7 +6,6 @@ public class Password {
     //시크릿키를 이용해 비밀번호를 encrypt 하는 메소드
     public static String encrypt(String rawPW){
         String encPW  = "" ;
-        /*TODO: make encryption logic*/
         int len = rawPW.length();
         for(int i=0;i<len;i++){
             encPW += (((secretKey.charAt(i % keyLen) ^ rawPW.charAt(i)) | i) * rawPW.charAt(i)) % 0xFF ;
