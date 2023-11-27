35a8d21575aa504932136b32f662b1eaa904272c
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-27 20:15:45 +0900

    feat : 로그인페이지 1차 완성

--- src/Main/gui/LoginPage.java
+++ src/Main/gui/LoginPage.java
@@ -1,4 +1,150 @@
 package Main.gui;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class LoginPage {
+public class LoginPage extends JFrame {
+    public LoginPage() {
+        setTitle("Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 800);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+
+        RoundedPanel backgroundPanel = new RoundedPanel(29, 44, 392, 700, new Color(150, 150, 150, 50));
+        mainPanel.add(backgroundPanel);
+
+        JLabel loginLabel = new JLabel("Login");
+        loginLabel.setBounds(52, 130, 78, 40);
+        loginLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        mainPanel.add(loginLabel);
+
+        createButton(mainPanel, "회원가입", 150, 600);
+        createButton(mainPanel, "로그인", 150, 530);
+
+        createStyledTextField(mainPanel, "비밀번호를 입력하세요", 52, 285, 340, 1);
+        createStyledTextField(mainPanel, "아이디를 입력하세요", 52, 200, 340, 2);
+
+        mainPanel.setFocusable(true); // Set focus to the main panel
+
+        add(mainPanel);
+        setVisible(true);
+    }
+
+    private void createButton(JPanel panel, String text, int left, int top) {
+        JButton button = new JButton(text);
+        button.setBounds(left, top, 60, 15);
+        button.setFont(new Font("Arial", Font.PLAIN, 15));
+        panel.add(button);
+    }
+
+    private void createStyledTextField(JPanel panel, String placeholder, int left, int top, int width, int id) {
+        StyledTextField textField = new StyledTextField(placeholder);
+        textField.setBounds(left, top + 20, width, 40); // Increased height
+        textField.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        panel.add(textField);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LoginPage());
+    }
+
+    class StyledTextField extends JTextField {
+        private String hintText;
+
+        public StyledTextField(String hintText) {
+            this.hintText = hintText;
+            setupUI();
+        }
+
+        private void setupUI() {
+            setOpaque(false);
+            setBorder(new CompoundBorder(new RoundedCornerBorder(), new EmptyBorder(0, 10, 0, 10)));
+            setForeground(Color.GRAY);
+            setText(hintText);
+
+            addFocusListener(new FocusListener() {
+                @Override
+                public void focusGained(FocusEvent e) {
+                    if (getText().equals(hintText)) {
+                        setText("");
+                        setForeground(Color.BLACK);
+                    }
+                }
+
+                @Override
+                public void focusLost(FocusEvent e) {
+                    if (getText().isEmpty()) {
+                        setText(hintText);
+                        setForeground(Color.GRAY);
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int arc = 15; // Corner radius
+            g2.setColor(getBackground());
+            g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, arc, arc);
+
+            super.paintComponent(g);
+        }
+    }
+
+    class RoundedCornerBorder extends AbstractBorder {
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int arc = 15; // Corner radius
+            g2.setColor(c.getBackground());
+            g2.drawRoundRect(x, y, width - 1, height - 1, arc, arc);
+
+            g2.dispose();
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(4, 8, 4, 8); // Adjust the padding as needed
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c, Insets insets) {
+            insets.set(4, 8, 4, 8); // Adjust the padding as needed
+            return insets;
+        }
+    }
+
+    class RoundedPanel extends JPanel {
+        private int x, y, width, height;
+        private Color color;
+
+        public RoundedPanel(int x, int y, int width, int height, Color color) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.color = color;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setColor(color);
+            g2d.fillRoundRect(x, y, width, height, 15, 15);
+            g2d.dispose();
+        }
+    }
 }
+
+
