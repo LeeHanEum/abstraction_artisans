79faee7c350bd100e98a2ea9b27653c425e293b5
Meonu <109051879+Meonu@users.noreply.github.com>
2023-12-03 15:23:38 +0900

    Mod UserListPage

--- src/Main/gui/UserListPage.java
+++ src/Main/gui/UserListPage.java
@@ -1,8 +1,10 @@
 package Main.gui;
 
 import Main.mall.Admin;
+import Main.mall.Item.GraphicsCard;
 import Main.mgr.Manageable;
 import Main.mall.User;
+import Main.mgr.Manager;
 
 import javax.security.sasl.SaslClient;
 import javax.swing.*;
@@ -10,6 +12,7 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.Naming;
 
 public class UserListPage extends JFrame{
     public UserListPage(Admin admin){
@@ -100,12 +103,18 @@ public class UserListPage extends JFrame{
     }
 
     private void createGroupPanel(JPanel parentPanel, User usr) {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(360,30));
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.X_AXIS));
+
+
         JPanel groupPanel = new JPanel();
-        groupPanel.setPreferredSize(new Dimension(360, 30)); // 너비 조절
+        groupPanel.setPreferredSize(new Dimension(280, 30)); // 너비 조절
         groupPanel.setBackground(Color.WHITE);
         groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.X_AXIS));
 
-
+        //초기 패딩
         groupPanel.add(Box.createRigidArea(new Dimension(30, 0)));
         // 하얀색 바탕의 레이블
         JLabel whiteLabel = new JLabel();
@@ -138,10 +147,39 @@ public class UserListPage extends JFrame{
 
 
         // 우측 여백 추가
-        groupPanel.add(Box.createRigidArea(new Dimension(298, 0)));
+        groupPanel.add(Box.createRigidArea(new Dimension(30, 0)));
+
+        mainPanel.add(groupPanel);
+
+        mainPanel.add(Box.createRigidArea(new Dimension(20, 30)));
+
+        mainPanel.add(Box.createRigidArea(new Dimension(5,30)));
+        //수정버튼
+        JPanel outerContainer = new JPanel();
+        outerContainer.setPreferredSize(new Dimension(40,30));
+        outerContainer.setLayout(null);
+        outerContainer.setBounds(0, 0, 40, 30);
+        outerContainer.setBackground(new Color(82, 82, 82));
+        mainPanel.add(outerContainer);
+
+        // Text container
+        JPanel textContainer = new JPanel();
+        textContainer.setLayout(null);
+        textContainer.setBounds(10, 0, 60, 30);
+        textContainer.setBackground(new Color(82, 82, 82));
+
+        JLabel labelText = new JLabel(" 주문내역");
+        labelText.setForeground(Color.WHITE);
+        labelText.setFont(new Font("Inter", Font.BOLD, 12));
+        labelText.setHorizontalAlignment(JLabel.LEFT);
+        labelText.setBounds(0, 0, 130, 30);
+        textContainer.add(labelText);
+        outerContainer.add(textContainer);
+
+
 
         // 부모 패널에 그룹 패널 추가
-        parentPanel.add(groupPanel);
+        parentPanel.add(mainPanel);
     }
 }
 
