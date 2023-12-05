8073037ffbab6430f9351fb446b4a96870e5ebad
석해현 <gogus4600@kyonggi.ac.kr>
2023-12-05 13:15:28 +0900

    장바구니 기능 수정 및 주석처리

--- src/Main/gui/ShoppingListPage.java
+++ src/Main/gui/ShoppingListPage.java
@@ -4,26 +4,42 @@ import Main.mall.Admin;
 import Main.mall.Item.Product;
 import Main.mall.Cart;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
+/*
+* 수정 추가할 기능
+* 수량 조절을 했을때 총 가격이 원래 가격 + 수량 조절된 가격으로 됨 예를 들어 원래 총가격이 286650 이였는데 제품 1개의 수량을 올리면
+* 수량을 올려서 조절된 총 가격 + 286650 의 결과로 총 가격이 나옴
+* +는 1개씩 늘어나는데 - 누르면 바로 1로 돌아옴
+* 체크 박스는 만들었는데 이걸 총 가격에 반환시키는 기능을 아직 못 만듦
+* 상품이 7개 이상 담기면 정보가 안 뜸
+* 같은 이름과 가격의 상품이 담기면 수량만 늘게 만드는 기능도 필요함
+*
+*
+* 쓰다보니 한게 없는거 같네,,,임시로 ltemlistpage에 부품 리스트에 장바구니 추가 넣어놨어
+* admin에 카드 클래스 불러올 수 있는 getCart() 함수도 만들어놨어
+*/
 public class ShoppingListPage extends JFrame {
 
     private final Cart cart;
     private final Admin admin;
     private final JPanel contentPanel;
     private int totalPrice = 0; // 총 가격을 저장할 변수
-    private JTextField quantityTextField;
+    private Map<Product, JTextField> productQuantityMap = new HashMap<>();
+
     public ShoppingListPage(Admin admin) {
         this.cart = admin.getCart();
         this.admin = admin;
 
         setTitle("모두나와 PC - 장바구니");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(450, 800); // Adjust the initial size as needed
+        setSize(450, 800); // 필요에 따라 초기 크기 조정
 
         JPanel cartPanel = new JPanel();
         cartPanel.setBackground(new Color(240, 240, 240));
@@ -34,7 +50,7 @@ public class ShoppingListPage extends JFrame {
         cartPanel.add(titleLabel, BorderLayout.NORTH);
 
         contentPanel = new JPanel();
-        contentPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // Use FlowLayout
+        contentPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // FlowLayout 사용
         JScrollPane scrollPane = new JScrollPane(contentPanel);
         scrollPane.getVerticalScrollBar().setUnitIncrement(30);
         customizeScrollBar(scrollPane);
@@ -58,13 +74,15 @@ public class ShoppingListPage extends JFrame {
     private void createProductPanels() {
         List<Product> productList = cart.getProductList();
 
-        // Create product panels based on the updated list
+        // 업데이트된 목록을 기반으로 제품 패널 생성
         for (Product product : productList) {
             System.out.println("Creating panel for product: " + product.getName());
             createProductPanel(product);
         }
-        updateTotalPriceLabel(); // 총 가격을 표시하는 레이블 추가
-        // Revalidate and repaint to reflect the changes
+
+        updateTotalPriceLabel(); // 각 제품에 대한 총 가격 라벨 업데이트
+
+        // 변경 사항을 반영하기 위해 다시 유효성 검사 및 다시 그림
         contentPanel.revalidate();
         contentPanel.repaint();
     }
@@ -83,7 +101,7 @@ public class ShoppingListPage extends JFrame {
         JLabel imageLabel = new JLabel();
         imageLabel.setPreferredSize(new Dimension(80, 80));
 
-        //이미지불러오기
+        // 이미지 불러오기
         ImageIcon imageIcon = new ImageIcon("src/Main/resource/productImage/" + product.getProductId() + "번.png");
         try {
             Image resizedImage = imageIcon.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
@@ -97,7 +115,7 @@ public class ShoppingListPage extends JFrame {
         textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
 
         // 텍스트패널 크기 조정
-        textPanel.setPreferredSize(new Dimension(345, 80)); // Adjust the width as needed
+        textPanel.setPreferredSize(new Dimension(345, 80)); // 필요에 따라 폭 조정
 
         JLabel nameLabel = new JLabel(product.getName());
         nameLabel.setFont(new Font("맑은 고딕", Font.BOLD, 20));
@@ -107,7 +125,7 @@ public class ShoppingListPage extends JFrame {
         priceLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         priceLabel.setForeground(new Color(40, 40, 40));
 
-        //버튼들
+        // 버튼들
         JPanel quantityPanel = new JPanel(); // 수량과 버튼들을 포함하는 패널
         quantityPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         quantityPanel.setAlignmentX(Component.LEFT_ALIGNMENT); // 왼쪽 정렬
@@ -115,22 +133,21 @@ public class ShoppingListPage extends JFrame {
         quantityPanel.setLayout(new BoxLayout(quantityPanel, BoxLayout.X_AXIS));
 
         JLabel quantityLabel = new JLabel("수량");
-        JTextField quantityTextField = new JTextField("1", 1);
 
-        quantityTextField.setPreferredSize(new Dimension(30, 20)); // Adjust width and height as needed
+        JTextField quantityTextField = new JTextField("1", 1);
+        productQuantityMap.put(product, quantityTextField);
+
+        quantityTextField.setPreferredSize(new Dimension(30, 20)); // 필요에 따라 너비와 높이 조정
 
         JButton increaseButton = new JButton("+");
         JButton decreaseButton = new JButton("-");
         JButton deleteButton = new JButton("삭제");
-        JCheckBox checkBox = new JCheckBox(); // Create a checkbox
+        JCheckBox checkBox = new JCheckBox(); // 체크박스 생성
+        checkBox.setSelected(true);
 
         int initialQuantity = Integer.parseInt(quantityTextField.getText());
         int initialTotalPrice = product.getPrice() * initialQuantity;
-
-        //박스 체크됐으면 더하는거
-        if (checkBox.isSelected()) {
-            totalPrice += initialTotalPrice;
-        }
+        totalPrice += initialTotalPrice;
 
         // quantityPanel에 컴포넌트들을 추가
         quantityPanel.add(quantityLabel);
@@ -138,35 +155,43 @@ public class ShoppingListPage extends JFrame {
         quantityPanel.add(increaseButton);
         quantityPanel.add(decreaseButton);
         quantityPanel.add(deleteButton);
-        quantityPanel.add(Box.createHorizontalGlue()); // Add horizontal glue for spacing
-        quantityPanel.add(checkBox); // Add the checkbox
+        quantityPanel.add(Box.createHorizontalGlue()); // 간격 조정을 위해 수평 glue 추가
+        quantityPanel.add(checkBox); // 체크박스 추가
 
         // quantityPanel의 크기를 조정
         quantityPanel.setPreferredSize(new Dimension(250, 30));
         // 컴포넌트들의 여백을 조정
         quantityPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        //수량조절
+        // 수량 조절
         increaseButton.addActionListener(e -> {
-            int quantity = Integer.parseInt(quantityTextField.getText());
+            int quantity = Integer.parseInt(productQuantityMap.get(product).getText());
             quantity++;
-            quantityTextField.setText(String.valueOf(quantity));
+            System.out.println("Increased quantity for product " + product.getName() + " to: " + quantity);
+
+            // 수량 맵 업데이트
+            productQuantityMap.put(product, quantityTextField);
+
+            // 총 가격 및 GUI 업데이트
             updateQuantityAndTotalPrice(product, quantity, checkBox.isSelected());
+            productQuantityMap.get(product).setText(String.valueOf(quantity));
         });
 
+        // decreaseButton의 action listener 내부
         decreaseButton.addActionListener(e -> {
-            int quantity = Integer.parseInt(quantityTextField.getText());
+            int quantity = Integer.parseInt(productQuantityMap.get(product).getText());
             if (quantity > 1) {
                 quantity--;
-                quantityTextField.setText(String.valueOf(quantity));
+                System.out.println("Decreased quantity for product " + product.getName() + " to: " + quantity);
+                productQuantityMap.get(product).setText(String.valueOf(quantity));
                 updateQuantityAndTotalPrice(product, quantity, checkBox.isSelected());
             }
         });
 
-        //삭제버튼
+        // 삭제 버튼
         deleteButton.setBackground(new Color(255, 0, 0));
         deleteButton.setForeground(new Color(255, 255, 255));
         deleteButton.addActionListener(e -> deleteItemFromCart(product));
-        deleteButton.setFocusPainted(false);  // Remove button focus border
+        deleteButton.setFocusPainted(false); // 버튼 포커스 테두리 제거
 
         textPanel.add(nameLabel);
         textPanel.add(priceLabel);
@@ -184,46 +209,45 @@ public class ShoppingListPage extends JFrame {
         contentPanel.add(productPanel);
         contentPanel.add(Box.createVerticalStrut(10)); // 수직 여백 추가
 
-        // Revalidate and repaint to reflect the changes
+        // 변경 사항을 반영하기 위해 다시 유효성 검사 및 다시 그림
         contentPanel.revalidate();
         contentPanel.repaint();
     }
 
-    //+,- 버튼 누르고 총 가격 업데이트, 체크박스 유무에 따라 더하게 하기
+    // +, - 버튼 누르고 총 가격 업데이트
     private void updateQuantityAndTotalPrice(Product product, int quantity, boolean isChecked) {
-        // Get the current total price of the product in the cart
-        int currentTotalPrice = product.getPrice() * Integer.parseInt(quantityTextField.getText());
+        // 현재 장바구니의 제품에 대한 현재 총 가격 가져오기
+        int currentTotalPrice = product.getPrice() * Integer.parseInt(productQuantityMap.get(product).getText());
+        System.out.println("Current total price for " + product.getName() + ": " + currentTotalPrice);
 
-        // Update the quantity in the cart
+        // 장바구니에서 수량 업데이트
         cart.editQuantityFromCart(product.getProductId(), quantity);
 
-        // Calculate the new total price
+        // 해당 제품에 대한 새로운 총 가격 계산
         int newTotalPrice = product.getPrice() * quantity;
+        System.out.println("New total price for " + product.getName() + ": " + newTotalPrice);
 
-        // Update the overall total price only if the checkbox is selected
-        if (isChecked) {
-            totalPrice += (newTotalPrice - currentTotalPrice);
-        }
+        // 전체 총 가격만 해당 제품에 대해 업데이트
+        totalPrice = totalPrice - currentTotalPrice + newTotalPrice;
+        System.out.println("Updated total price: " + totalPrice);
 
-        // Update the total price label
-        updateTotalPriceLabel();
-
-        // Update the shopping list page
+        // 쇼핑 목록 페이지 업데이트
         updateShoppingListPage();
+        // 총 가격 라벨 업데이트
+        updateTotalPriceLabel();
     }
 
-
-
-    //총가격 라벨
+    // 총 가격 라벨
     private void updateTotalPriceLabel() {
         JLabel totalPriceLabel = new JLabel("총 가격: " + totalPrice + "원");
         totalPriceLabel.setFont(new Font("맑은 고딕", Font.BOLD, 18));
         totalPriceLabel.setForeground(Color.BLACK);
 
-        // Replace the existing total price label with the updated one
+        // 기존 총 가격 라벨을 업데이트된 라벨로 교체
         contentPanel.add(totalPriceLabel, contentPanel.getComponentCount() - 1);
     }
-    //장바구니 삭제
+
+    // 장바구니에서 항목 삭제
     private void deleteItemFromCart(Product product) {
         Iterator<Product> iterator = cart.getProductList().iterator();
         while (iterator.hasNext()) {
@@ -234,37 +258,39 @@ public class ShoppingListPage extends JFrame {
                 updateShoppingListPage();
                 updateTotalPriceLabel();
 
-                return;  // Assuming each product ID is unique; if not, remove the return statement
+                return;  // 각 제품 ID가 고유하다고 가정; 그렇지 않으면 return 문 제거
             }
         }
     }
-    //페이지 update하기
+
+    // 페이지 업데이트
     private void updateShoppingListPage() {
         List<Product> productList = cart.getProductList();
 
-        // Clear the content panel
+        // content panel 비우기
         contentPanel.removeAll();
 
-        // Create product panels based on the updated list
+        // 업데이트된 목록을 기반으로 제품 패널 생성
         if (productList.isEmpty()) {
-            // If the cart is empty, display a message
+            // 장바구니가 비어 있으면 메시지 표시
             JLabel emptyCartLabel = new JLabel("장바구니가 비었습니다.");
             emptyCartLabel.setFont(new Font("맑은 고딕", Font.BOLD, 18));
             emptyCartLabel.setForeground(Color.BLACK);
             contentPanel.add(emptyCartLabel);
         } else {
-            // Create product panels based on the updated list
+            // 업데이트된 목록을 기반으로 제품 패널 생성
             for (Product product : productList) {
                 System.out.println("Creating panel for product: " + product.getName());
                 createProductPanel(product);
             }
         }
 
-        // Revalidate and repaint to reflect the changes
+        // 변경 사항을 반영하기 위해 다시 유효성 검사 및 다시 그림
         contentPanel.revalidate();
         contentPanel.repaint();
     }
-    //스크롤바 커스터마이즈
+
+    // 스크롤바 커스터마이즈
     private void customizeScrollBar(JScrollPane scrollPane) {
         JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
         verticalScrollBar.setUI(new BasicScrollBarUI() {
@@ -275,12 +301,13 @@ public class ShoppingListPage extends JFrame {
             }
         });
     }
-    //마이페이지로
+
+    // 마이페이지로 이동
     private void navigateToMainPage() {
-        // Close the current window
+        // 현재 창 닫기
         dispose();
 
-        // Open the main page
+        // 메인 페이지 열기
         new MyPage(admin);
     }
 }
